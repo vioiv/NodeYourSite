52577c7b1ca52aa48b7e814a8ad2b18876ce6e9b
JeongHoon Kim <devkjh@gmail.com>
2018-12-20 10:28:35 +0900

    add rest api

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.listen(8000, () => {
     console.log('Server started!');
 });
+
+app.route('/api/shopping/reviews').get((req, res) => {
+    res.send({
+        reviews: [{ review: 'bag'}, { review: 'computer'}]
+    });
+});
